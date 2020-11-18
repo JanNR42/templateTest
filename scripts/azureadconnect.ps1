9d98a3d5d3556ce677c95d90f8ec56c7600d6643
MrIsaac42 <61246838+MrIsaac42@users.noreply.github.com>
2020-11-18 15:35:57 +0100

    Add files via upload

---
+++ scripts/azureadconnect.ps1
@@ -0,0 +1,9 @@
+$AADConnectDLUrl = "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
+$tempfile = [System.IO.Path]::GetTempFileName()
+$folder = [System.IO.Path]::GetDirectoryName($tempfile)
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile($AADConnectDLUrl, $tempfile)
+Rename-Item -Path $tempfile -NewName "AzureADConnect.msi"
+$MSIPath = $folder + "\AzureADConnect.msi"
+Copy-Item -Path $MSIPath -Destination "C:\Users\Public\Desktop"
+
